b686b106431f59cac96362f953b7039f21fb7e51
soumya098 <soumyarj904@gmail.com>
2023-10-29 15:55:41 +0530

    mongodb connection fix

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,12 +5,16 @@ let isConnected: boolean = false;
 export const connectToDB = async () => {
 	mongoose.set('strictQuery', true);
 
+	if (!process.env.MONGODB_URL) {
+		return console.log('MISSING MONGODB_URL');
+	}
+
 	if (isConnected) {
 		return console.log('<<<<<< MongoDB is already connected >>>>>>');
 	}
 
 	try {
-		await mongoose.connect(process.env.MONGODB_URL, { dbName: 'devflow', useNewUrlParser: true, useUnifiedTopology: true });
+		await mongoose.connect(process.env.MONGODB_URL, { dbName: 'devflow' });
 
 		isConnected = true;
 		console.log('<<<<<< MongoDB is connected >>>>>>');
