248a3765d6ebcd8e01b9a0a7371a248fb6af3386
soumya098 <soumyarj904@gmail.com>
2023-11-02 20:13:00 +0530

    implement all tags page

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,35 @@
+import TagCard from '@/components/cards/TagCard';
+import Filter from '@/components/shared/Filter';
+import NoResult from '@/components/shared/NoResult';
+import LocalSearchBar from '@/components/shared/search/LocalSearchBar';
+import { TagFilters } from '@/constants/filters';
+import { getAllTags } from '@/lib/actions/tag.action';
+import React from 'react';
+
+const Tag = async () => {
+	const result = await getAllTags({});
+
+	return (
+		<>
+			<h1 className='h1-bold text-dark100_light900'> Tags</h1>
+
+			<div className='mt-10 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+				<LocalSearchBar route='/tags' iconPosition='left' imgSrc='/assets/icons/search.svg' placeholder='Search for Tags' otherClasses='flex-1' />
+
+				<div>
+					<Filter options={TagFilters} otherClasses='min-h-[56px] sm:min-w-[170px]' />
+				</div>
+			</div>
+
+			<section className='mt-10 flex w-full flex-wrap gap-4'>
+				{result.tags.length > 0 ? (
+					result.tags.map((tag) => <TagCard key={tag._id} tag={tag} />)
+				) : (
+					<NoResult title='No Tags Found' desc='It looks like there are no tags available' link='/ask-question' linkText='Ask a Question' />
+				)}
+			</section>
+		</>
+	);
+};
+
+export default Tag;
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,7 +2,22 @@
 
 import User from '@/database/user.model';
 import { connectToDB } from '../mongoose';
-import { GetTopInteractedTagsParams } from './shared';
+import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared';
+import Tag from '@/database/tag.model';
+
+export async function getAllTags(params: GetAllTagsParams) {
+	try {
+		await connectToDB();
+		// const {page, filter, searchQuery, pageSize } = params
+
+		const tags = await Tag.find({});
+
+		return { tags };
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+}
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 	try {
